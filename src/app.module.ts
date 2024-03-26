ee417566ca736527e54e3086276761b8cde66f8e
pichead <61868523+pichead@users.noreply.github.com>
2024-03-27 01:04:59 +0700

    add webhook

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
+import { WebhookModule } from './webhook/webhook.module';
 
 @Module({
-  imports: [AdminModule],
+  imports: [AdminModule, WebhookModule],
   controllers: [AppController],
   providers: [AppService],
 })
